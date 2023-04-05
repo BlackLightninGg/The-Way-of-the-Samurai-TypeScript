624592c6397cf8ab0cf087f9cc2351762160b691
Aleksey <easy.mail.to.copy@gmail.com>
2023-04-05 22:46:06 +0300

    Prepare html template component Message

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,8 +1,30 @@
+import { NavLink } from 'react-router-dom'
+import s from './Messages.module.css'
+
 
 export const Messages = () => {
   return (
-    <div>
-      Messages
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialogItem}>
+          <NavLink to='/fox' className={navData => navData ? s.active : s.dialogItem}>Fox</NavLink>
+        </div>
+        <div className={s.dialogItem}>
+          <NavLink to='/bear' className={navData => navData ? s.active : s.dialogItem}>Bear</NavLink>
+        </div>
+        <div className={s.dialogItem}>
+          <NavLink to='/owl' className={navData => navData ? s.active : s.dialogItem}>Owl</NavLink>
+        </div>
+        <div className={s.dialogItem}>
+          <NavLink to='/wolf' className={navData => navData ? s.active : s.dialogItem}>Wolf</NavLink>
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.messageItem}>Frrr fr fr</div>
+        <div className={s.messageItem}>Raaaaa</div>
+        <div className={s.messageItem}>Woooo wo</div>
+        <div className={s.messageItem}>Woyyyyy woyyyy</div>
+      </div>
     </div>
   )
 }
