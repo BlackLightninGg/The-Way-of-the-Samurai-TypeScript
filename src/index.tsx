6cef44983cac89dfae5627cadb5637eb5462ba3d
Алексей <73067946+BlackLightninGg@users.noreply.github.com>
2023-06-13 11:44:31 +0000

    Created deep and shallow copy  state inside messagesReducer and profileReducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,18 @@
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import './index.css';
-// import { store } from "./redux/store";
-import {reduxStore} from "./redux/reduxStore";
+import { reduxStore } from "./redux/reduxStore";
 
 
-const renderEntireTree = () => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={reduxStore}>
-                <App/>,
-            </Provider>
-        </BrowserRouter>,
-        document.getElementById('root')
-    );
-}
-reduxStore.subscribe(renderEntireTree)
-renderEntireTree()
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={reduxStore}>
+            <App />,
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
+
